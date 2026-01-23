7e687260204d2b9ece15c634704530164847c1f7
StasKazakov <urgor8@gmail.com>
2026-01-23 18:43:40 +0200

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,9 @@
-import DataTable from '@/components/DataTable'
-import Image from 'next/image'
-import Link from 'next/link'
-import { cn, formatCurrency } from '@/lib/utils'
-import { TrendingUp, TrendingDown } from 'lucide-react'
-import { fetcher } from '@/lib/coingecko.actions'
-
-const columns: DataTableColumn<TrendingCoin>[] = [
-  { 
-    header: "Name", 
-    cellClassName: "name-cell", 
-    cell: (coin) =>  {
-      const item = coin.item;
-      return (
-        <Link href={`/coins/${item.id}`}>
-          <Image src={item.large} alt={item.name} width={36} height={36} />
-          <p>{item.name}</p>
-        </Link>
-      )
-    },
-  },
-    {
-      header: '24h Change',
-      cellClassName: "change-cell",
-      cell: (coin) => {
-        const item = coin.item;
-        const isTrendingUp = item.data.price_change_percentage_24h.usd > 0
-        return (
-          <div className={cn("price-change", isTrendingUp ? "text-green-500" : "text-red-500")}>
-            <p>
-              {isTrendingUp ? (
-                <TrendingUp width={16} height={16}/>
-              ) : (
-                <TrendingDown width={16} height={16}/>)}
-            </p>
-          </div>
-        )
-      }
-    },
-    { header: "Price", cellClassName: "price-cell", cell: (coin) => coin.item.data.price },
-]
 
 const page = async () => {
-  const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
-    dex_pair_format: "symbol"
-  })
-
-  const trandingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300)
   
   return (
     <main className='main-container'>
       <section className='home-grid'>
-        <div id='coin-overview'>
-          <div className='header pt-2'>
-            <Image src={coin.image.large} 
-            alt={coin.name} 
-            width={56} 
-            height={56} />
-            <div className='info'>
-              <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
-              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-            </div>
-          </div>
-        </div>
-
-        <p>Trending Coins</p>
-        <DataTable
-        data={[]} 
-        columns={[{ header: "Title"}, { header: "Price"}]}/>
       </section>
 
       <section className='w-full mt-7 space-y-4'>
